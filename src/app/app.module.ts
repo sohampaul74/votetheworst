ad8873e63882e56294e749c8540187b0a7a968b3
soham paul <riddhi.sohampaul@gmail.com>
2021-05-28 13:25:11 +0530

    initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './common/header/app-header/app-header.component';
+import { EntryComponent } from './common/header/entry/entry.component';
+import { LandingComponent } from './common/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    EntryComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
